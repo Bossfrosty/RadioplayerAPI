98f819a937b9d52f2bb0ef902675d8a4f38478b3
Jailson Bezerra <jailson.sb2@gmail.com>
2024-06-24 16:39:32 -0300

    updates

--- index.php
+++ index.php
@@ -105,10 +105,11 @@
                             Historico
                         </button>
                         <button class="btn" data-outside="offcanvas-stations">
-                            <svg class="i i-radio" viewBox="0 0 24 24">
+                            <svg class="i i-list" viewBox="0 0 24 24">
                                 <circle cx="12" cy="12" r="2"></circle>
                                 <path d="M5 4.9a10 10 0 0 0 0 14.2M7.8 7.7a6 6 0 0 0 0 8.6m8.4 0a6 6 0 0 0 0-8.6M19 19.1a10 10 0 0 0 0-14.2"></path>
                             </svg>
+                            Multiradio
                         </button>
                         <button class="btn l:none" data-outside="mobile-menu">
                             <svg class="i i-bars" viewBox="0 0 24 24">
